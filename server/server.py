55dd968de489a89f39167eddff7498deb200b942
Andrew Mellen <andrew_mellen@icloud.com>
2017-11-15 20:30:02 -0500

    try to update the html

--- server/server.py
+++ server/server.py
@@ -1,4 +1,5 @@
 import socket
+import re
 
 
 TCP_PORT = 8000
@@ -10,6 +11,26 @@ s.listen(1)
 conn, addr = s.accept()
 
 data = conn.recv(BUFFER_SIZE)
-print(data.decode('utf-8'))
+
+data = data.decode('utf-8')
+data = data.split()
+
+name = data[0]
+ip = data[1]
+
+
+final_file = ''
+with open('index.html', 'r') as f:
+    for line in f:
+        if name in line:
+            regex = r'[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]'
+            line_ip = re.findall(regex, line)[0]
+            final_file += line.replace(line_ip, ip)
+        else:
+            final_file += line
+
+with open('index.html', 'w') as f:
+    for line in final_file:
+        f.write(line)
 
 conn.close()
