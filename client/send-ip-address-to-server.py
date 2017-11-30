252f6452db930ffb190d97635c1a5de682120ca3
Andrew Mellen <andrew_mellen@icloud.com>
2017-11-30 00:17:02 -0500

    Basic Encryption working
    Also made a bunch of functions to make both scripts very expandable

--- client/send-ip-address-to-server.py
+++ client/send-ip-address-to-server.py
@@ -1,18 +1,122 @@
 import netifaces as ni
 import socket
+import json
+from Crypto.PublicKey import RSA
+from Crypto import Random
+import base64
 
 
-def get_ip():
-    return ni.ifaddresses('en0')[ni.AF_INET][0]['addr']
+def get_ip(device):
+    """
+    Return the ip adress of device
+    """
+    return ni.ifaddresses(device)[ni.AF_INET][0]['addr']
 
 
-ip = get_ip()
+def encode_base64_ascii(the_bytes):
+    """
+    Encode the byte array in base 64 and then decode into a ascii str
+
+    Return an ascii str of the base64 encoded bytes
+    """
+    encoded_b64 = base64.b64encode(the_bytes)
+    return encoded_b64.decode('ascii')
+
+
+def encrypt_password(publickey, password):
+    """
+    Encrypt the password and encodes it with encode_base64_ascii
+
+    Return the encrypted and encoded string
+    """
+    # Encrypt get first value of tuple because docs said second value is
+    # always None for some reason
+    encoded_pass = publickey.encrypt(password.encode('utf-8'), 32)[0]
+    # Encode to b64 ascii
+    encoded_pass_b64str = encode_base64_ascii(encoded_pass)
+    return encoded_pass_b64str
+
+
+def receive_json_message(conn):
+    """
+    Receive a json message given a connection socket
+
+    Return the json data
+    """
+    # Client sends size of next message first
+    message_size = int(conn.recv(6).decode('ascii'))
+    # Get message given message_size
+    rec_data = conn.recv(message_size)
+    # Decode it as utf-8 and load as json
+    data_str = rec_data.decode('utf-8')
+    return json.loads(data_str)
+
+
+def create_message(request, name=None, ip=None, password=None):
+    """
+    Creates a json message to send to the server
+
+    requests:
+    - PUBLICKEY - get the public key
+    - UPDATEIP - send an update for the ip
+    - DONE - close the connection
+    """
+    dictionary = {'request': request}
+    if name is not None:
+        dictionary['name'] = name
+    if ip is not None:
+        dictionary['ip'] = ip
+    if password is not None:
+        dictionary['password'] = password
+    return json.dumps(dictionary).encode('utf-8')
+
+
+def send_message(s, message):
+    """
+    Send a message
+    First sends the length of the message (bytes) and then sends the message.
+    The sizeofmessage is always going to be 6 bytes, so the server can always
+    know the size of that message.
+    """
+    # send sizeofmessage message first
+    sizeofmessage = len(message)
+    sizeofmessage = '{0:06}'.format(sizeofmessage)
+    sizeofmessage = sizeofmessage.encode('ascii')
+    s.send(sizeofmessage)
+    # Send actual message
+    s.send(message)
+
+
+# Use a larger key length in practice...
+KEY_LENGTH = 2048  # Key size (in bits)
+random_gen = Random.new().read
+
+# Generate RSA private/public key pairs for both parties...
+keypair = RSA.generate(KEY_LENGTH, random_gen)
+
+# Stuff about the server
+ip = get_ip('en0')
 TCP_IP = 'localhost'
 TCP_PORT = 8000
-BUFFER_SIZE = 1024
+# TODO Temporary, not sure how I'm going to set password yet...
+PASSWORD = 'stukmakenpassword'
 
+# Create the socket, connecting to the server
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((TCP_IP, TCP_PORT))
-message = b'Stukmaken ' + ip.encode('utf-8')
-s.send(message)
+# Ask for the public key
+message = create_message('PUBLICKEY')
+send_message(s, message)
+# Get public key and make a RSA object with it
+publickey_str = receive_json_message(s)['publickey']
+publickey = RSA.importKey(publickey_str)
+# Get the encoded password
+encoded_pass_b64str = encrypt_password(publickey, PASSWORD)
+# Send the update ip message
+message = create_message('UPDATEIP', 'Stukmaken', ip, encoded_pass_b64str)
+send_message(s, message)
+# Send that it's done
+message = create_message('DONE')
+send_message(s, message)
+# Close the connection
 s.close()
--- server/server.py
+++ server/server.py
@@ -1,23 +1,111 @@
 import socket
 import re
-from Crypto.Cipher import AES
+from Crypto.PublicKey import RSA
+from Crypto import Random
 from threading import Thread, Lock
+import json
+import base64
+
+
+def generate_keys():
+    """
+    Returns a keypair
+    """
+    # Use a larger key length in practice...
+    KEY_LENGTH = 2048  # Key size (in bits)
+    random_gen = Random.new().read
+
+    # Generate RSA private/public key pairs for both parties...
+    keypair = RSA.generate(KEY_LENGTH, random_gen)
+    return keypair
+
+
+# Create keypair
+keypair = generate_keys()
+# Public Key as a utf-8 str
+publickey = keypair.publickey().exportKey().decode('utf-8')
+
+
+def decrypt_password(b64encrypted):
+    """
+    Decrypt the password. Sent as ascii encoded base64 data
+    First encode the byte string as ascii, then decode the b64,
+    then decrypt.
+
+    Return the password as a utf-8 str
+    """
+    # base64ascii back to bytes
+    b64bytes = b64encrypted.encode('ascii')
+    encrypted_pass = base64.b64decode(b64bytes)
+    return keypair.decrypt(encrypted_pass).decode('utf-8')
+
+
+def receive_json_message(conn):
+    """
+    Receive a json message given a connection socket
+
+    Return the json data
+    """
+    # Client sends size of next message first
+    message_size = int(conn.recv(6).decode('ascii'))
+    # Get message given message_size
+    rec_data = conn.recv(message_size)
+    # Decode it as utf-8 and load as json
+    data_str = rec_data.decode('utf-8')
+    return json.loads(data_str)
+
+
+def send_message(s, message):
+    """
+    Send a message
+    First sends the length of the message (bytes) and then sends the message.
+    The sizeofmessage is always going to be 6 bytes, so the server can always
+    know the size of that message.
+    """
+    sizeofmessage = len(message)
+    sizeofmessage = '{0:06}'.format(sizeofmessage)
+    sizeofmessage = sizeofmessage.encode('ascii')
+    s.send(sizeofmessage)
+    s.send(message)
 
 
 def client_handler(conn, addr):
     """
     Handle a client message
     """
-    data = conn.recv(BUFFER_SIZE)
-    data = data.decode('utf-8')
-    data = data.split()
+    while True:
+        data = receive_json_message(conn)
 
-    name = data[0]
-    ip = data[1]
-    print(name + ' ' + ip)
-    write_to_file(name, ip)
+        if 'request' not in data:
+            # Means that this is a bad message
+            return
+        if data['request'] == 'PUBLICKEY':
+            # Send this client the public key
+            message_dict = {'publickey': publickey}
+            message = json.dumps(message_dict).encode('utf-8')
+            send_message(conn, message)
+        elif data['request'] == 'UPDATEIP':
+            # Bad message if none of these keys are in the dict
+            if 'name' not in data:
+                return
+            if 'ip' not in data:
+                return
+            if 'password' not in data:
+                return
+            # Get the info
+            name = data['name']
+            ip = data['ip']
+            # TODO Figure out how to check if password is valid
+            password = decrypt_password(data['password'])
+            print(name + ' ' + ip + ' ' + password)
+            write_to_file(name, ip)
+        elif data['request'] == 'DONE':
+            # Close the socket
+            conn.close()
+            break
 
 
+# Lock for the write_to_file method
 file_lock = Lock()
 
 
@@ -54,14 +142,15 @@ Start the server
 """
 
 TCP_PORT = 8000
-BUFFER_SIZE = 1024
 
+# Set up the socket to listen
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind(('', TCP_PORT))
 s.listen()
 while True:
+    # Keep accepting clients and make a new thread for each
     conn, addr = s.accept()
     Thread(target=client_handler, args=(conn, addr)).start()
 
-
+# Close the socket if it gets out of the while loop
 conn.close()
